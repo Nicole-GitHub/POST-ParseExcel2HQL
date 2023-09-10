9c7fb0efa4e34764bf37f9c769479b9f670bd499
Nicole-GitHub <sandy770403@gmail.com>
2023-09-10 17:30:34 +0800

    u

--- src/main/java/gss/ETLCode/bin/DM_L02_LoadDM.java
+++ src/main/java/gss/ETLCode/bin/DM_L02_LoadDM.java
@@ -15,7 +15,7 @@ public class DM_L02_LoadDM {
 				+ "set hivevar:Run_TableName="+tableName+";\n"
 				+ "set hivevar:RUN_NOW="+mapProp.get("hadoop.std.dbname")+".SYS_RUN_NOW;\n"
 				+ "set hivevar:SRC1_L="+mapProp.get("hadoop.tmp.dbname")+".tmp_"+tableName+";\n"
-				+ "set hivevar:SRC2_BACKUP_DW="+mapProp.get("hadoop.tmp.dbname")+".BKD_"+tableName+";\n"
+				+ "set hivevar:SRC2_BACKUP_DM="+mapProp.get("hadoop.tmp.dbname")+".BKD_"+tableName+";\n"
 				+ "set hivevar:DES1="+mapProp.get("hadoop.raw.dbname")+"."+tableName+";\n"
 				+ "-----------------------------------------------------------------\n"
 				+ "\n"
@@ -23,8 +23,18 @@ public class DM_L02_LoadDM {
 				+ "INSERT OVERWRITE TABLE ${hivevar:DES1}\n"
 				+ (!StringUtils.isBlank(partition) ? "PARTITION(" + partition + ") \n" : "")
 				+ "select * \n"
-				+ "from ${hivevar:SRC1_L}\n"
-				+ ";" ;
+				+ "from ${hivevar:SRC1_L}\n";
+		
+			rs += StringUtils.isBlank(partition) ?
+				"\n"
+				+ "-- 無partition但又需保留舊資料時 \n"
+				+ "UNION ALL \n"
+				+ "\n"
+				+ "select a.* \n"
+				+ "FROM ${hivevar:SRC2_BACKUP_DM}_${hivevar:BATCHID} a\n"
+				+ "join (select ymds from ${hivevar:RUN_NOW} where trim(tablenm) = '${hivevar:Run_TableName}') run\n"
+				+ "on [*** ACT_YM ***] <> run.ymds\n"
+				+ ";" : ";";
 
 		return rs;
 	}
@@ -38,7 +48,7 @@ public class DM_L02_LoadDM {
 				+ "set hivevar:Run_TableName="+tableName+";\n"
 				+ "set hivevar:RUN_NOW="+mapProp.get("hadoop.std.dbname")+".SYS_RUN_NOW;\n"
 				+ "set hivevar:SRC1_L="+mapProp.get("hadoop.tmp.dbname")+".tmp_"+tableName+";\n"
-				+ "set hivevar:SRC2_BACKUP_DW="+mapProp.get("hadoop.tmp.dbname")+".BKD_"+tableName+";\n"
+				+ "set hivevar:SRC2_BACKUP_DM="+mapProp.get("hadoop.tmp.dbname")+".BKD_"+tableName+";\n"
 				+ "set hivevar:DES1="+mapProp.get("hadoop.raw.dbname")+"."+tableName+";\n"
 				+ "-----------------------------------------------------------------\n"
 				+ "\n";
