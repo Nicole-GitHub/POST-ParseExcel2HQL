9672242481ce6c47cc543c9ccde826777bdd7e78
Nicole-GitHub <sandy770403@gmail.com>
2023-09-11 10:53:20 +0800

    update flow remove BACKUP_DW.hql

--- src/main/java/gss/ETLCode/flow.java
+++ src/main/java/gss/ETLCode/flow.java
@@ -12,11 +12,10 @@ public class flow {
 			rs = "1:BEFORE_C03_ExportDate2File.sh\n"
 				+ "2:BEFORE_C04_FinishData.sh\n"
 				+ "3:FILE_UPLOAD.sh\n"
-				+ "4:BACKUP_DW.hql\n"
-				+ "5:TRUNCATE_DW.sh\n"
-				+ "6:DW_L07_LoadDW.hql\n"
-				+ "7:DW_L08_LoadDW_TMP.hql\n"
-				+ "8:FINISH.hql\n";
+				+ "4:TRUNCATE_DW.sh\n"
+				+ "5:DW_L07_LoadDW.hql\n"
+				+ "6:DW_L08_LoadDW_TMP.hql\n"
+				+ "7:FINISH.hql\n";
 		} else {
 			rs = "1:BEFORE_C01_Run.hql\n"
 				+ "2:BEFORE_C02_Check.hql\n"
@@ -46,12 +45,11 @@ public class flow {
 			rs = "1:BEFORE_C03_ExportDate2File.sh\n"
 				+ "2:BEFORE_C04_FinishData.sh\n"
 				+ "3:FILE_UPLOAD.sh\n"
-				+ "4:BACKUP_DW.hql\n"
-				+ "5:TRUNCATE_DW.sh\n"
-				+ "6:DW_L07_LoadDW.hql\n"
-				+ "7:DW_L08_LoadDW_TMP.hql\n"
-				+ "8:DW_EXPORT_2SQL.sh\n"
-				+ "9:FINISH.hql\n";
+				+ "4:TRUNCATE_DW.sh\n"
+				+ "5:DW_L07_LoadDW.hql\n"
+				+ "6:DW_L08_LoadDW_TMP.hql\n"
+				+ "7:DW_EXPORT_2SQL.sh\n"
+				+ "8:FINISH.hql\n";
 		} else {
 			rs = "1:BEFORE_C01_Run.hql\n"
 				+ "2:BEFORE_C02_Check.hql\n"
