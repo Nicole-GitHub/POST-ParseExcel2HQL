e5eabe09c5bac45f86cfa51fcf6a611e109858ed
Nicole-GitHub <sandy770403@gmail.com>
2023-12-07 17:35:53 +0800

    u

--- src/main/java/gss/Word/ParseControlSheet.java
+++ src/main/java/gss/Word/ParseControlSheet.java
@@ -20,7 +20,8 @@ public class ParseControlSheet {
 	public static List<Map<String, String>> run(String fileNamePath, List<String> fileENameList) throws Exception {
 		Map<String, String> map = new HashMap<String, String>();
 		List<Map<String, String>> mapList = new ArrayList<Map<String, String>>();
-
+		List<String> include = new ArrayList<String>();
+		
 		try {
 		
 			fileNamePath += "POST第2階段ETL開發管控.xlsx";
@@ -47,32 +48,35 @@ public class ParseControlSheet {
 				// 只整理這次要解析的table即可
 				if (fileENameList.contains(targetTableEName)) {
 					
-					String targetTableCName = POITools.getCellValue(row, c++, "中文檔案名稱").toUpperCase();
-					c++; // 舊檔案名稱
-					String sourceTableEName = POITools.getCellValue(row, c++, "來源檔").toUpperCase();
-					String sourceTableENameNoExt = sourceTableEName.indexOf(".") > 0
-							? sourceTableEName.substring(0, sourceTableEName.lastIndexOf("."))
-							: sourceTableEName;
-					c++; // 來源檔(公式)
-					c++; // 狀態
-					c++; // Owner
-					c++; // 預計完成日
-					c++; // 完成日
-					c++; // 測試資料提供
-					String dataTransferInterval = POITools.getCellValue(row, c++, "資料轉檔區間").toUpperCase();
-
-					// 來源檔若為.ps則需更改副檔名為.txt
-					if (sourceTableEName.toUpperCase().endsWith(".PS"))
-						sourceTableEName = sourceTableEName.substring(0, sourceTableEName.length() - 3) + ".txt";
-
-					map = new HashMap<String, String>();
-					map.put("targetTableEName", targetTableEName);
-					map.put("targetTableCName", targetTableCName);
-					map.put("sourceTableEName", sourceTableEName);
-					map.put("sourceTableENameNoExt", sourceTableENameNoExt);
-					map.put("dataTransferInterval", dataTransferInterval);
-
-					mapList.add(map);
+					if(!include.contains(targetTableEName)) {
+						include.add(targetTableEName);
+						String targetTableCName = POITools.getCellValue(row, c++, "中文檔案名稱").toUpperCase();
+						c++; // 舊檔案名稱
+						String sourceTableEName = POITools.getCellValue(row, c++, "來源檔").toUpperCase();
+						String sourceTableENameNoExt = sourceTableEName.indexOf(".") > 0
+								? sourceTableEName.substring(0, sourceTableEName.lastIndexOf("."))
+								: sourceTableEName;
+						c++; // 來源檔(公式)
+						c++; // 狀態
+						c++; // Owner
+						c++; // 預計完成日
+						c++; // 完成日
+						c++; // 測試資料提供
+						String dataTransferInterval = POITools.getCellValue(row, c++, "資料轉檔區間").toUpperCase();
+	
+						// 來源檔若為.ps則需更改副檔名為.txt
+						if (sourceTableEName.toUpperCase().endsWith(".PS"))
+							sourceTableEName = sourceTableEName.substring(0, sourceTableEName.length() - 3) + ".txt";
+						
+						map = new HashMap<String, String>();
+						map.put("targetTableEName", targetTableEName);
+						map.put("targetTableCName", targetTableCName);
+						map.put("sourceTableEName", sourceTableEName);
+						map.put("sourceTableENameNoExt", sourceTableENameNoExt);
+						map.put("dataTransferInterval", dataTransferInterval);
+	
+						mapList.add(map);
+					}
 				}
 			}
 		
