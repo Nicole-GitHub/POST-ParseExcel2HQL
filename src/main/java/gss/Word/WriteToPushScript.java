18595598dea3778df6ae856a6fdb1d3d5841120f
Nicole-GitHub <sandy770403@gmail.com>
2023-12-21 13:56:57 +0800

    u

--- src/main/java/gss/Word/WriteToPushScript.java
+++ src/main/java/gss/Word/WriteToPushScript.java
@@ -14,6 +14,7 @@ public class WriteToPushScript {
 		try {
 
 			String targetTableEName = "", 
+				sourceTableEName = "",
 				mkdirDownloadFolder = "", 
 				mkdirHadoopFolder = "", 
 				chmod = "", 
@@ -32,30 +33,42 @@ public class WriteToPushScript {
 				runNowE = "", 
 				insertRunNow = "", 
 				runType = "", 
-				pipeShell = "";
+				pipeShell = "",
+				selectSourceScript = "",
+				filePath = "",
+				gssSQLConn = "";
 			
 			for(Map<String, String> controlSheetMap : controlSheetList) {
 
 				targetTableEName = controlSheetMap.get("targetTableEName");
+				sourceTableEName = controlSheetMap.get("sourceTableEName");
 
 				if("1".equals(mapProp.get("runType"))){
+					// mkdir Download Folder
 					mkdirDownloadFolder += "mkdir /home/post1/DW_WORK/download/" + targetTableEName + "\n"
 							+"mkdir /home/post1/DW_WORK/download/" + targetTableEName + "/BACKUP\n"
 							+"mkdir /home/post1/DW_WORK/download/" + targetTableEName + "/DONE\n"
 							+"mkdir /home/post1/DW_WORK/download/" + targetTableEName + "/WORK\n\n";
+					
+					// mkdir Hadoop Folder
 					mkdirHadoopFolder += "gssShell fs -mkdir /user/post1/Upload/tmp/" + targetTableEName + "/\n";
-					zip += controlSheetMap.get("sourceTableEName").startsWith("T_") ? "" 
+					
+					// zip
+					zip += sourceTableEName.startsWith("T_") ? "" 
 							: "zip -jmqP POST@23931261 /home/post1/DW_WORK/download/"+controlSheetMap.get("sourceTableENameNoExt")+".zip "
-								+"/home/post1/DW_WORK/download/"+controlSheetMap.get("sourceTableEName") + "\n";
+								+"/home/post1/DW_WORK/download/"+ sourceTableEName + "\n";
 				}
 
+				// chmod
 				chmod += "cd /opt/gss/pipe-logic-deploy/post/" + targetTableEName + "\n"
 						+"chmod 777 main\n"
 						+"cp flow.def_noExport.txt flow.def\n\n";
+				
+				// Result Table
 				createRSLT += "create table post1_post_poc_tmp." + targetTableEName + "_result "
 						+ "like post1_post_poc_tmp.T_CMMW_FSMTR_C_TEMP_result; \n"; 
 				
-				
+				// SYS_RUN_INFO
 				dataTransferInterval = controlSheetMap.get("dataTransferInterval").toUpperCase();
 				runInfo = "X".equals(dataTransferInterval) ? "'A',0,0"
 								: "系統年".equals(dataTransferInterval) ? "'Y',0,0"
@@ -69,6 +82,7 @@ public class WriteToPushScript {
 				insertRunInfo += "INSERT OVERWRITE TABLE post1_post_poc_std.SYS_RUN_INFO PARTITION(TABLENM) "
 						+"SELECT "+runInfo+", CURRENT_TIMESTAMP, '" + targetTableEName + "';\n";
 				
+				// SYS_RUN_NOW
 				yyyy = Tools.getNOW("yyyy");
 				yyyyMM = Tools.getNOW("yyyyMM");
 				yyyyMMDD = Tools.getNOW("yyyyMMdd");
@@ -95,10 +109,20 @@ public class WriteToPushScript {
 				
 				insertRunNow += "INSERT OVERWRITE TABLE post1_post_poc_std.SYS_RUN_NOW PARTITION(TABLENM) "
 						+"SELECT '" + runNowS + "', '" + runNowE + "', CURRENT_TIMESTAMP, '" + targetTableEName + "';\n";
+				
+				// pipeShell
 				runType = "X".equals(dataTransferInterval) ? "--var RUN_TYPE=A"
 							: "--var RUN_TYPE=M --var DATA_S=" + runNowS + " --var DATA_E=" + runNowE;
 				pipeShell += "pipe-shell --logic-uri /opt/gss/pipe-logic-deploy/post/" + targetTableEName + " "
 						+ runType + " --is-staged --is-bypass-pre-check --clear-cache \n";
+				
+				// Select Source Table Script
+				selectSourceScript = "select * from post1_post_poc_raw."+ sourceTableEName +"  t ;";
+				
+				// gssSQLConn
+				filePath = "/opt/gss/pipe-logic-deploy/post/";
+				gssSQLConn = "gssSQLConn --user hdfs --logic-file " + filePath + targetTableEName + "/bin/DM_T01.hql 1> " + filePath + targetTableEName + "/log/01.txt 2>&1\n"
+						+ "gssSQLConn --user hdfs --logic-file " + filePath + targetTableEName + "/bin/DM_L03_LoadDM.hql 1> " + filePath + targetTableEName + "/log/03.txt 2>&1\n\n"; 
 
 			}
 			
@@ -113,7 +137,9 @@ public class WriteToPushScript {
 					+ "\n--createRSLT\n" + createRSLT
 					+ "\n--insertRunInfo\n" + insertRunInfo
 					+ "\n--insertRunNow\n" + insertRunNow
-					+ "\n--pipeShell\n" + pipeShell ;
+					+ "\n--pipeShell\n" + pipeShell
+					+ "\n--selectSourceScript\n" + selectSourceScript
+					+ "\n--gssSQLConn\n" + gssSQLConn ;
 			
 			FileTools.createFileNotAppend(outputPath, "PushScript", "sql", rs);
 
